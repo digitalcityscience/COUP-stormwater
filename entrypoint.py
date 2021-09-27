5a18ebdd771dde1a65b8b45c1061b0cc699a9b68
andredaa <post.for.andre@gmail.com>
2021-09-27 12:14:02 +0200

    Add Flask-Cors to entrypoint

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,11 @@
+from flask_cors import CORS, cross_origin
 from endpoints import app
 
+# Description
+# This file is only to be executed for development purpose. It provides more debugging (maybe also sensitive) ]
+# informations. For production please use it with gunicorn (see entrypoint.sh)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    app.run(host='0.0.0.0', port=5002, debug=True)
